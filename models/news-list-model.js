66a0a18bbe8bd11c6a6e747730e9f47e23e3e37c
Rob Holden <robholdenflag@gmail.com>
2017-02-16 16:18:59 +0000

    Blacked out, then I had some work... It did'nt work

---
+++ models/news-list-model.js
@@ -0,0 +1,62 @@
+"use strict";
+
+(function(exports) {
+  var NewsList = function() {
+    this.list = [];
+  };
+
+  NewsList.prototype.getData = function() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlHTTP.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200) {
+        myData = JSON.parse(this.responeText);
+    }
+  };
+
+  xmlhttp.open("GET", "http://content.guardianapis.com/uk?show-editors-picks=true&show-fields=body&api-key=aa4d0029-5dc9-4ac6-86f9-d34f382c4d8e", true);
+  xmlhttp.send();
+  delayAssignment(this);
+  };
+
+  function delayAssignment (item) {
+    setTimeout(function() {
+      item.addNews();
+    }, 1000);
+  }
+
+  //
+  // NewsList.prototype.addNews = function () {
+  //   for ( var i = 0; i <  )
+  //
+  //   this.list.push()
+  // }
+
+exports.NewsList = NewsList;
+
+})(this);
+
+
+// {
+// -response: {
+// status: "ok"
+// userTier: "developer"
+// total: 1930143
+// startIndex: 1
+// pageSize: 10
+// currentPage: 1
+// pages: 193015
+// orderBy: "newest"
+// -results: [
+// -{
+// id: "sport/2017/feb/16/mercedes-f1-title-hopes-boosted-by-appointment-of-james-allison-technical-director"
+// type: "article"
+// sectionId: "sport"
+// sectionName: "Sport"
+// webPublicationDate: "2017-02-16T11:23:43Z"
+// webTitle: "Mercedes’ F1 title hopes boosted by appointment of James Allison"
+// webUrl: https://www.theguardian.com/sport/2017/feb/16/mercedes-f1-title-hopes-boosted-by-appointment-of-james-allison-technical-director
+// apiUrl: https://content.guardianapis.com/sport/2017/feb/16/mercedes-f1-title-hopes-boosted-by-appointment-of-james-allison-technical-director
+// isHosted: false
+//     };
+//   };
+// };
--- models/news-model.js
+++ models/news-model.js
@@ -1,41 +1,15 @@
-"use strict";
-
 (function(exports) {
 
-  function News() {
-    // this.news = news;
+  var News = function(headline, body) {
+    this.headline = headline;
+    this.body = body;
+
   };
 
-  Note.prototype.getNews = function () {
-    ;
-  }
+  News.prototype.viewHeadline = function(){
+    return this.headline;
+  };
 
 exports.News = News;
 
 })(this);
-
-
-// {
-// -response: {
-// status: "ok"
-// userTier: "developer"
-// total: 1930143
-// startIndex: 1
-// pageSize: 10
-// currentPage: 1
-// pages: 193015
-// orderBy: "newest"
-// -results: [
-// -{
-// id: "sport/2017/feb/16/mercedes-f1-title-hopes-boosted-by-appointment-of-james-allison-technical-director"
-// type: "article"
-// sectionId: "sport"
-// sectionName: "Sport"
-// webPublicationDate: "2017-02-16T11:23:43Z"
-// webTitle: "Mercedes’ F1 title hopes boosted by appointment of James Allison"
-// webUrl: https://www.theguardian.com/sport/2017/feb/16/mercedes-f1-title-hopes-boosted-by-appointment-of-james-allison-technical-director
-// apiUrl: https://content.guardianapis.com/sport/2017/feb/16/mercedes-f1-title-hopes-boosted-by-appointment-of-james-allison-technical-director
-// isHosted: false
-//     };
-//   };
-// };
